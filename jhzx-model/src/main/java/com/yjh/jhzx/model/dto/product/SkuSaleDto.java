8da07aaba14cbeccf74f042aa3ed6094922537be
yjh <1304877912@qq.com>
2024-05-06 17:48:47 +0800

    day1

---
+++ jhzx-model/src/main/java/com/yjh/jhzx/model/dto/product/SkuSaleDto.java
@@ -0,0 +1,12 @@
+package com.yjh.jhzx.model.dto.product;
+
+import lombok.Data;
+
+@Data
+public class SkuSaleDto {
+
+	private Long skuId;
+	private Integer num;
+
+}
+
---
+++ jhzx-model/src/main/java/com/yjh/jhzx/model/entity/system/SysRoleUser.java
@@ -0,0 +1,12 @@
+package com.yjh.jhzx.model.entity.system;
+
+import com.yjh.jhzx.model.entity.base.BaseEntity;
+import lombok.Data;
+
+@Data
+public class SysRoleUser extends BaseEntity {
+
+    private Long roleId;       // 角色id
+    private Long userId;       // 用户id
+
+}
