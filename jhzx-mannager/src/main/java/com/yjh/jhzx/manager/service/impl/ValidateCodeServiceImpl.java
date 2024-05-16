f2954a44ef3b4e3caaca7fa5af2dc70340dae632
yjh <1304877912@qq.com>
2024-05-16 17:51:26 +0800

    day1

--- jhzx-mannager/src/main/java/com/yjh/jhzx/manager/service/impl/ValidateCodeServiceImpl.java
+++ jhzx-mannager/src/main/java/com/yjh/jhzx/manager/service/impl/ValidateCodeServiceImpl.java
@@ -1,10 +1,16 @@
 package com.yjh.jhzx.manager.service.impl;
 
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.CircleCaptcha;
 import com.yjh.jhzx.manager.service.ValidateCodeService;
 import com.yjh.jhzx.model.vo.system.ValidateCodeVo;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ValidateCodeServiceImpl implements ValidateCodeService {
@@ -13,6 +19,12 @@ public class ValidateCodeServiceImpl implements ValidateCodeService {
 
     @Override
     public ValidateCodeVo generateValidateCode() {
+        //二维码
+        CircleCaptcha circleCaptcha = CaptchaUtil.createCircleCaptcha(150, 48, 4, 2);
+        String codeValue = circleCaptcha.getCode();//验证码的值
+        val imageBase64 = circleCaptcha.getImageBase64();
+        String key = UUID.randomUUID().toString().replaceAll("_","");
+        redisTemplate.opsForValue().set("user:validate"+key,codeValue,5, TimeUnit.MINUTES);
 
         return null;
     }
